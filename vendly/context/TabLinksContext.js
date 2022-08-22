cabc5186729f62f2a9a51f4928141115d852543a
Chimeruzee <itzmeruz31@gmail.com>
2022-08-22 13:32:50 +0100

    remove tabslink console log

--- vendly/context/TabLinksContext.js
+++ vendly/context/TabLinksContext.js
@@ -5,7 +5,6 @@ export const TabListContext = createContext(0);
 
 const TabLinkContextProvider = (props) => {
   const [tabIndex, dispatch] = useReducer(TabLinkReducer, 0);
-  console.log(tabIndex, "tabIndex");
 
   return (
     <TabListContext.Provider value={{ tabIndex, dispatch }}>
